de7312a838854237f1f791c131354d2996d81aaa
Spencer Smith <dontaskspencer@gmail.com>
2017-05-04 20:41:55 -0600

    Not using words

--- js/script.js
+++ js/script.js
@@ -3,21 +3,7 @@ var first;
 var position = 0;
 var marginBottom;
 
-var index = 0;
-var reviews = [
-	"We love this place! Whenever we are downtown this is probably where we will eat. We love the food. I would last or favorites but you can't really go wrong. Buy some sausage to take home and check out the candy.",
-  "It's basically little germany in the middle of SLC. Great lunch options, a full butcher shop, bakery and a store in the back with all your favorite german brands!",
-  "Delicious German food that always leaves me satisfied. I recommend their bratwurst (sandwich or otherwise).",
-  "Amazing service! Amazing outdoor seating although we came in winter. Indoors was cute, great ambience for a dinner with friends or a dinner date. Loved the cheeseboard and thin bread it comes with.",
-  "My all time favorite restaurant. I kind of don't want to tell people how great this place is because I'd hate for it to get so busy that I can't get in. The food is marvelous, the wine scrumptious, and the patio space in the summer is phenomenal. I love this restaurant!",
-  "This place had amazing food and the patio was so cute!  highly recommend sitting outside when the weather is nice.  Make sure to stop by the book store as well!",
-  "Great place! I took my family here for dinner before going to a show near by.  Erin our server and Shell the manager bent over backwards to provide us a fantastic, quick, and enjoyable dining experience. They well exceeded our expectations. Casual dining with great food. Thank you, ladies!",
-  "There are a lot of Mexican restaurants that really only serve American Mexican food. If you want original Mexican, The Red Iguana is your place. Forget the décor, you are there for the food. And it's great.",
-  "Absolutely delicious, authentic Italian. The Arista dish is amazing! Great service! Didn't know this place has been around for over 20 years! They also run the BTG Wine Bar a couple doors down.",
-  "Great food and lots of it. Easy to find. Quiant.",
-  "Amazing Italian food in a cozy setting. I would recommend making reservations if you plan on going on the weekend. During the summer they also have patio tables open.",
-  "Service was just ok, but the food outweighed that, well worth the bill."
-];
+var toggle = false;
 
 showInitialReviews();
 scrollUp();
@@ -34,7 +20,10 @@ function scrollUp(){
 
   first = document.querySelector('.review');
   // Use (position += .5)
-  first.style.marginTop = "-" + (position += .5) + "px";
+  if(toggle){
+    first.style.marginTop = "-" + (position += 1) + "px";
+  }
+  toggle = !toggle;
 
   window.requestAnimationFrame(scrollUp);
 }
@@ -49,11 +38,9 @@ function showReview(){
   }
 
   var words = createDiv('words');
-  words.innerText = reviews[index++];
-  if(index >= reviews.length){
-  	index = 0;
-  }
-
+  words.appendChild(createDiv('line'));
+  words.appendChild(createDiv('line'));
+  //words.appendChild(createDiv('line'));
 
   //review.innerText = ++count;
   review.appendChild(stars);
@@ -63,7 +50,7 @@ function showReview(){
 }
 
 function showInitialReviews(){
-	for(var i = 0; i < 6; i++){
+	for(var i = 0; i < 8; i++){
   	showReview();
   }
   var s = window.getComputedStyle(first);
